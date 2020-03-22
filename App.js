4520a2d5b2dd6d77783dac95541917c495e311d5
Akash Maurya <09.akash.maurya@gmail.com>
2020-03-22 22:49:41 +0530

    Learne : To Do App (part1)

--- App.js
+++ App.js
@@ -1,31 +1,28 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
+import Header from './components/header'
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: 'Akash', id: '1'},
-    { name: 'Aditya', id: '2'},
-    { name: 'Dev', id: '3'},
-    { name: 'Srajan', id: '4'},
-    { name: 'Shreyas', id: '5'},
-    { name: 'Buddy', id: '6'},
-  ]);
-
-  const pressHandler = (id) => {
-    console.log(id);
-  }
+  const [todos, setTodos] = useState([
+    {text: 'buy coffee', key: 1 },
+    {text: 'create an app', key: 2 },
+    {text: 'play on the switch', key: 3 }
+  ])
 
   return (
     <View style={styles.container}>
-      <FlatList 
-        keyExtractor = { (item) => item.id }
-        data = {people}
-        renderItem = { ({ item }) => (
-          <TouchableOpacity onPress = { () => pressHandler(item.id)}>
-            <Text style = {styles.item}> {item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
+      <Header />   
+      <View style = {styles.content}>
+        {/* to do form */}
+        <View style = {styles.list}>
+          <FlatList 
+            data = {todos} 
+            renderItem = {({ item }) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>  
+      </View>    
     </View>
   );
 }
@@ -34,16 +31,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20,
     // alignItems: 'center',
     // justifyContent: 'center',
   },  
-
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor:'lightblue',
-    fontSize: 24,
+  content : {
+    padding: 40,
+  },
+  list: {
+    margin: 20,
   }
 });
