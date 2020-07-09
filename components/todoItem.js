6509a5519f969d7e740912bd48e687953bbce23c
Akash Maurya <09.akash.maurya@gmail.com>
2020-07-09 15:04:07 +0530

    TODO app done

--- components/todoItem.js
+++ components/todoItem.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import React from 'react'
+import {StyleSheet, TouchableOpacity, Text} from 'react-native';
 
-export default function TodoItem({ item , pressHandler}) {
-    return (
-        <TouchableOpacity onPress = {() => pressHandler(item.key)}>
-            <Text style = {styles.item}>{item.text}</Text>
-        </TouchableOpacity>
-    )
+export default function TodoItem({ pressHandler, item }) {
+  return (
+    <TouchableOpacity onPress={() => pressHandler(item.key)}>
+      <Text style={styles.item}>{item.text}</Text>
+    </TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({
-    item: {
-        padding: 16,
-        marginTop: 16,
-        borderColor: '#bbb',
-        borderWidth: 1,
-        borderStyle: 'dashed',
-        borderRadius: 10
-    },
+  item: {
+    padding: 16,
+    marginTop: 16,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderRadius: 1,
+    borderRadius: 10,
+  }
 });
